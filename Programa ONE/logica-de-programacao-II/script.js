7d60b3f40915b99465a5b616330b8340b10d2148
Lucas Ferreira <lks_ferreira@hotmail.com>
2022-04-26 20:12:18 -0300

    Update script.js

--- Programa ONE/logica-de-programacao-II/script.js
+++ Programa ONE/logica-de-programacao-II/script.js
@@ -5,12 +5,15 @@ let pincel = canvas.getContext("2d")
 pincel.fillStyle = "lightgray"
 pincel.fillRect(0, 0, 600, 400);
 
+//Retangulo verde
 pincel.fillStyle = "green"
 pincel.fillRect(0, 0, 200, 400);
 
+//Retangulo vermelho
 pincel.fillStyle = "red"
 pincel.fillRect(400, 0, 200, 400);
 
+// Triangulo amarelo
 pincel.fillStyle = "yellow"
 pincel.beginPath();
 pincel.moveTo(300, 200);
@@ -18,6 +21,7 @@ pincel.lineTo(200, 400);
 pincel.lineTo(400, 400);
 pincel.fill();
 
+//Circulo azul
 pincel.fillStyle = "blue";
 pincel.beginPath();
 pincel.arc(300,200,50,0,2*3.14);
